d74cdbbd18dbbf68c9f40af44ae30f2795ec16b1
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2020-03-07 21:56:30 -0500

    built base
    animation, character movement, character action, bullet deflection

---
+++ TeamPrototype2/Assets/bulletPhysics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletPhysics : MonoBehaviour
+{
+    public Rigidbody2D bullet;
+    private Vector2 movement = new Vector2(1f, 0f);
+    public int moveSpeed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet.velocity = movement * moveSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TeamPrototype2/Assets/playerBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject player;
+    public int rotationIndex = 15;
+    private Collider2D bullet;
+    public LayerMask bulletsLayer;
+    public Transform deflectPoint;
+    public float dPointSize = 0.4f;
+    Vector2 mousePosition;
+    Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        RotatoPotato();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Swing");
+            SwingerOfThings();
+        }
+    }
+
+    void RotatoPotato()
+    {
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+
+    }
+    void SwingerOfThings()
+    {
+        bullet = Physics2D.OverlapCircle(deflectPoint.position, dPointSize, bulletsLayer);
+        if(bullet != null)
+        {
+            Transform bulletLocation = bullet.GetComponent<bulletPhysics>().bullet.transform;
+            bullet.GetComponent<bulletPhysics>().bullet.velocity = direction * 100 *Time.deltaTime;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(deflectPoint.position, dPointSize);
+    }
+}
